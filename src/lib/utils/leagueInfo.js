a8820620ff0bb86184ecd063951f2a4c90684aca
romphi <38505673+romphi@users.noreply.github.com>
2026-02-03 19:36:30 +0100

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -27,10 +27,10 @@ https://github.com/nmelhado/league-page/blob/master/TRAINING_WHEELS.md#ii-adding
 
 export const managers = [
     {
-      "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
+     // "roster": 1,  // ID of the roster that the manager manages (look at the order of the power rankings graph)
       "managerID" : "125244372861060710",
       "name": "Phil",
-      "tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
+      //"tookOver": 2020, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Brooklyn", // (optional)
       "bio": "Lorem ipsum...",
       "photo": "/managers/name.jpg", // square ratio recommended (no larger than 500x500)
